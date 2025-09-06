ece50571c772ccba522e587c8c027f8c6411a7e8
fabri <fabrisio021@gmail.com>
2025-09-06 12:50:42 -0500

    backend: added user controller, getRecommendedUsers

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,21 @@
+import User from "../models/User.js";
+// import FriendRequest from "../models/FriendRequest.js";
+
+export async function getRecommendedUsers(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = req.user;
+
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } }, 
+        { _id: { $nin: currentUser.friends } },
+        { isOnboarded: true },
+      ],
+    });
+    res.status(200).json(recommendedUsers);
+  } catch (error) {
+    console.error("Error in getRecommendedUsers controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
