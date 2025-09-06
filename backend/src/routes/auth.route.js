a899c578a00a2b0ee122a0662900d21140f2c2ee
fabri <fabrisio021@gmail.com>
2025-09-06 12:49:25 -0500

    backend: added user route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -8,11 +8,10 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-// router.post("/onboarding", protectRoute, onboard);
-// 
+router.post("/onboarding", protectRoute, onboard);
 
-// router.get("/me", protectRoute, (req, res) => {
-//   res.status(200).json({ success: true, user: req.user });
-// });
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
 export default router;
