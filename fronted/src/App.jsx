b71c5b84042d89fa092ab7a282d6f6c20241cd95
fabri <fabrisio021@gmail.com>
2025-09-06 14:54:48 -0500

    Fronted:added react-query,axios

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -7,6 +7,12 @@ function App() {
     <div className="h-screen">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call/:id" element={<CallPage />} />
+        <Route path="/chat/:id" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
     </div>
   );
