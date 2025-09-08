1b3329ccf3222deef737c0ffad171991419e3796
fabri <fabrisio021@gmail.com>
2025-09-08 11:30:56 -0500

    backend : added total ports

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
